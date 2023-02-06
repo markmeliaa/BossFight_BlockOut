48fd274e7fb685ddb914113cf7412f4ba76345c0
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-02-06 17:26:31 +0100

    Add respawn to the blockout

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerRespawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.transform.position = playerRespawn.transform.position;
+            player.transform.rotation = playerRespawn.transform.rotation;
+            Physics.SyncTransforms();
+        }
+    }
+}
