6b6f40a732f391e3e4d812927d37a5d623539f68
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2023-02-19 22:56:44 +0100

    level andres
    Co-Authored-By: AndresM2 <61988590+AndresM2@users.noreply.github.com>

--- Assets/Scripts/DestroyArena.cs
+++ Assets/Scripts/DestroyArena.cs
@@ -30,6 +30,6 @@ public class DestroyArena : MonoBehaviour
     IEnumerator DestroyFloor()
     {
         yield return new WaitForSeconds(2.0f);
-        octogonalFloor.GetComponent<Animator>().SetBool("falling", true);
+        Destroy(octogonalFloor);
     }
 }
